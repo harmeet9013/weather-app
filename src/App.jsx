a14db8d1fd7c4c68f2dbe6e4fdecf7b1c01f8e8e
Harmeet Singh <evildoremon@gmail.com>
2023-06-11 19:36:31 +0530

    Added description and credits.

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,16 @@ export default function App() {
                     WeatherInfo={WeatherInfo}
                 />
             </Box>
+            <p className="desc-para">
+                This website is a weather app that allows users to search for
+                the current weather conditions of any city in the world. It uses
+                the OpenWeatherMap API to fetch the data and displays it in a
+                simple and elegant interface. The website is built with ReactJS,
+                a popular JavaScript library for creating user interfaces. The
+                website also features a dark mode option and a responsive design
+                that adapts to different screen sizes.
+            </p>
+            <h2 className="created-by">by Harmeet Singh</h2>
         </Container>
     );
 }
